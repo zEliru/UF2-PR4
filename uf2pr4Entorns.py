f7b81612446f6786cd18ddaa833d5522dee5273b
Edu <pewpet@gmail.com>
2023-02-06 18:01:19 +0100

    first commit

---
+++ uf2pr4Entorns.py
@@ -0,0 +1,79 @@
+#imports here
+import os 
+#top 
+def eduFunction():
+    print("hola soc l'edu")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#edu until here
+def olegFunction():
+    print("fuck you")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#oleg until here
+
+
+
+def startOfGame():
+    isValid = False
+    name = str(input("Hello. What is your name?"))
+    print(f"Hi {name}.")
+    while (not isValid):
+        
+        choice = int(input("You are a giraffe and you are walking on a sand road. \
+            Your path is divided. Would you like to go left(1) or right(2)?"))
+        if (choice == 1):
+            isValid=True
+            eduFunction()
+        elif (choice == 2):
+            isValid=True
+            olegFunction()
+        else:
+            print("Error. Please choose wisely. Either 1 or 2. Please.")
+
+#here starts the game
+startOfGame()
+#game ends
+
+
+input()
+
+
+
+
+
+
+
+
+
+
