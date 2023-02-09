a2c6db4f576003a85584554c96465bf23036646c
Oleg <a2208021@institutmontilivi.cat>
2023-02-09 16:23:12 +0100

    Hola edu, este es mi primer commit en VSCode

--- uf2pr4Entorns.py
+++ uf2pr4Entorns.py
@@ -1,5 +1,5 @@
 #imports here
-import os 
+import os, msvcrt 
 #top 
 def eduFunction():
     print("hola soc l'edu")
@@ -21,11 +21,51 @@ def eduFunction():
 
 
 #edu until here
+
 def olegFunction():
-    print("fuck you")
+    print("You've choosen the RIGTH path")
+    print("Your game narrated by Oleg starts here!")
+    print("You open your eyes, you where kidnapped...")
+    print("you realize that now you aren't in the zoo of NY...")
+    print("you are in the african jungle.")
+    input()
+    os.system('cls')
+    print("PRESS START!")
+    msvcrt.getch()
+    os.system('cls')
+    print("Welcome to the jungle!")
+    print("You see two roads and have to choose one of them pressing the number they have.")
+    print("The first road starts with a field of flowers with butterflies flying around. (1)")
+    print("The second path is a field of rocks (2)")
+    isValid = False
+    choice = int(input("Which one do you choose?"))
+    while(not isValid):
+        if (choice == 1): # the path of flowers
+            isValid=True
+            olegPathOneOne()
+        elif (choice == 2): # the path of rocks
+            isValid=True
+            olegPathOneTwo()
+        else:
+            print("Error. Please choose wisely. Either 1 or 2. Please.")
+    msvcrt.getch()
+    os.system('cls')
 
+#os.system('cls')
 
+def olegPathOneOne():
+    print("Not a good choice!")
+    print("You forgot about your allergies...")
+    print("you can't breath... a giraffe in trouble!")
+    msvcrt.getch()
+    os.system('cls')
 
+def olegPathOneTwo():
+    print("Ohhh! good choice!")
+    print("The rocks are made from marshmellow...")
+    print("you're having a sweet time.")
+    msvcrt.getch()
+    os.system('cls')
 
 
 
